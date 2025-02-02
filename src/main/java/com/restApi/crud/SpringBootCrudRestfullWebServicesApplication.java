663eb9a038d6b0791905f1015c1a369661c45d12
Amit Kumar Pandey <amitkumar.pandey052001@gmail.com>
2025-02-02 22:42:11 +0530

    Initail Commit

---
+++ src/main/java/com/restApi/crud/SpringBootCrudRestfullWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.restApi.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudRestfullWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudRestfullWebServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/restApi/crud/SpringBootCrudRestfullWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restApi.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestfullWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
