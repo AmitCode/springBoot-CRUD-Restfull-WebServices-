80c783a46c2094a06717f0d08f6e4919c51cd1ac
Amit Kumar Pandey <amitkumar.pandey052001@gmail.com>
2025-02-09 19:57:23 +0530

    Getter and Setter Issue fixed in IntelliJ IDEA

--- src/main/java/com/restApi/crud/pojo/Book.java
+++ src/main/java/com/restApi/crud/pojo/Book.java
@@ -20,11 +20,84 @@ public class Book {
     String bookName;
     String bookAuthor;
     String bookSubject;
-//    @UpdateTimestamp
-//    String lastIssueDate;
-//    String issuedBy;
-//    String issuedById;
-//    String issuedToUserName;
-//    String issuedToUserId;
+    @UpdateTimestamp
+    String lastIssueDate;
+String issuedBy;
+    String issuedById;
+    String issuedToUserName;
+    String issuedToUserId;
+
+//    public String getIssuedBy() {
+//        return issuedBy;
+//    }
+//
+//    public void setIssuedBy(String issuedBy) {
+//        this.issuedBy = issuedBy;
+//    }
+//
+//    public String getIssuedById() {
+//        return issuedById;
+//    }
+//
+//    public void setIssuedById(String issuedById) {
+//        this.issuedById = issuedById;
+//    }
+//
+//    public String getIssuedToUserName() {
+//        return issuedToUserName;
+//    }
+//
+//    public void setIssuedToUserName(String issuedToUserName) {
+//        this.issuedToUserName = issuedToUserName;
+//    }
+//
+//    public String getIssuedToUserId() {
+//        return issuedToUserId;
+//    }
+//
+//    public void setIssuedToUserId(String issuedToUserId) {
+//        this.issuedToUserId = issuedToUserId;
+//    }
+//
+//    public String getIssuedToUserEmail() {
+//        return issuedToUserEmail;
+//    }
+//
+//    public void setIssuedToUserEmail(String issuedToUserEmail) {
+//        this.issuedToUserEmail = issuedToUserEmail;
+//    }
+//
 //    String issuedToUserEmail;
+//
+//    public Long getBookId() {
+//        return bookId;
+//    }
+//
+//    public void setBookId(Long bookId) {
+//        this.bookId = bookId;
+//    }
+//
+//    public String getBookName() {
+//        return bookName;
+//    }
+//
+//    public void setBookName(String bookName) {
+//        this.bookName = bookName;
+//    }
+//
+//    public String getBookAuthor() {
+//        return bookAuthor;
+//    }
+//
+//    public void setBookAuthor(String bookAuthor) {
+//        this.bookAuthor = bookAuthor;
+//    }
+//
+//    public String getBookSubject() {
+//        return bookSubject;
+//    }
+//
+//    public void setBookSubject(String bookSubject) {
+//        this.bookSubject = bookSubject;
+//    }
 }
